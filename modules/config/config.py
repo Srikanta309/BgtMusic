2439888398985841731682353a251b6f6ea53a2b
Srikanta309 <139033378+Srikanta309@users.noreply.github.com>
2023-07-11 17:18:23 +0530

    Update config.pyj

--- modules/config/config.py
+++ modules/config/config.py
@@ -91,7 +91,7 @@ LOG_FILE_NAME = "adityalogs.txt"
 
 
 # All Images Of Bikash Aditya Player // @BIKASHHALDER @AdityaHalder
-START_IMG_URL = getenv("START_IMG_URL", f"https://te.legra.ph/file/99d0261f0aa5512ad6753.jpg")
+START_IMG_URL = getenv("START_IMG_URL", f"IMG_20230711_170816_643.jpg")
 PING_IMG_URL = getenv("PING_IMG_URL", "resource/ping.jpeg")
 PLAYLIST_IMG_URL = getenv("PLAYLIST_IMG_URL", "resource/playlist.jpeg")
 GLOBAL_IMG_URL = getenv("GLOBAL_IMG_URL", "resource/global.jpeg")
